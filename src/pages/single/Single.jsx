8ae2088bfa66c629749ac73889a958d33b5c0d6f
prahladtimalsina <prahladtimalsina7@gmail.com>
2022-03-25 23:02:58 +0545

    single page last transactions done

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -2,6 +2,7 @@ import './single.scss'
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
 const Single = () => {
   return (
     <div className="single">
@@ -43,7 +44,10 @@ const Single = () => {
             <Chart aspect={3 / 1} title="User Spending ( List 6 Months )" />
           </div>
         </div>
-        <div className="buttom"></div>
+        <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <List />
+        </div>
       </div>
     </div>
   );
