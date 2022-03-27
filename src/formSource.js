e9e054a00fcebd6b8b76678607532a9777641694
prahladtimalsina <prahladtimalsina7@gmail.com>
2022-03-27 22:16:13 +0545

    form input api calls

---
+++ src/formSource.js
@@ -0,0 +1,79 @@
+export const userInputs = [
+  {
+    id: 1,
+    label: "Username",
+    type: "text",
+    placeholder: "prahlad_timalsina",
+  },
+  {
+    id: 2,
+    label: "Name and surname",
+    type: "text",
+    placeholder: "prahlad_timalsina",
+  },
+  {
+    id: 3,
+    label: "Email",
+    type: "mail",
+    placeholder: "prahladtimalsina7@gmail.com",
+  },
+  {
+    id: 4,
+    label: "Phone",
+    type: "text",
+    placeholder: "+977-9843166540",
+  },
+  {
+    id: 5,
+    label: "Password",
+    type: "password",
+  },
+
+  {
+    id: 6,
+    label: "Address",
+    type: "text",
+    placeholder: "Lalitpur",
+  },
+
+  {
+    id: 7,
+    label: "Country",
+    type: "text",
+    placeholder: "Nepal",
+  },
+];
+
+export const productInputs = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "Apple Macbook pro",
+  },
+
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "Computers",
+  },
+  {
+    id: 4,
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+  {
+    id: 5,
+    label: "Stock",
+    type: "text",
+    placeholder: "in stock",
+  },
+];
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,20 +1,27 @@
-import "./new.scss"
+import { useState } from "react";
+import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-const New = () => {
+
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
   return (
     <div className="new">
       <Sidebar />
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1>Add New User</h1>
+          <h1>{title}</h1>
         </div>
         <div className="bottom">
           <div className="left">
             <img
-              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
               alt=""
             />
           </div>
@@ -24,36 +31,19 @@ const New = () => {
                 <label htmlFor="file">
                   Image: <DriveFolderUploadOutlinedIcon className="icon" />
                 </label>
-                <input type="file" id="file" style={{ display: "none" }} />
+                <input
+                  type="file"
+                  onChange={e => setFile(e.target.files[0])}
+                  id="file"
+                  style={{ display: "none" }}
+                />
               </div>
-              <div className="formInput">
-                <label>Username</label>
-                <input type="text" placeholder="prahlad_timalsina" />
-              </div>
-              <div className="formInput">
-                <label>Name and surname</label>
-                <input type="text" placeholder="prahlad_timalsina" />
-              </div>
-              <div className="formInput">
-                <label>Email</label>
-                <input type="email" placeholder="prahladtimalsina7@gmail.com" />
-              </div>
-              <div className="formInput">
-                <label>Phone</label>
-                <input type="text" placeholder="+977-12346879" />
-              </div>
-              <div className="formInput">
-                <label>Password</label>
-                <input type="password" />
-              </div>
-              <div className="formInput">
-                <label>Address</label>
-                <input type="text" placeholder="Lalitpur , nepal" />
-              </div>
-              <div className="formInput">
-                <label>Country</label>
-                <input type="text" placeholder="Nepal" />
-              </div>
+              {inputs.map(input => (
+                <div className="formInput" key={input.key}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
               <button>Send</button>
             </form>
           </div>
@@ -63,4 +53,4 @@ const New = () => {
   );
 };
 
-export default New
+export default New;
